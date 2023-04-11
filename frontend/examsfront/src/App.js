38e9b7b708861e23e7a09e9ed4a27241d221d95c
nanakwafo <nanamensah1140@gmail.com>
2023-04-11 06:01:06 +0000

    Added video stream using cloudfront

--- frontend/examsfront/src/App.js
+++ frontend/examsfront/src/App.js
@@ -5,12 +5,15 @@ import Filmmaker from './pages/filmmaker'
 import Home from './pages/home'
 import Login from './pages/login'
 import Register from './pages/register'
+import Filmviewer from './pages/filmviewer'
 function App() {
   return <Routes>
+    <Route path="/home" element={ <Home/>} />
     <Route path="/" element={ <Home/>} />
     <Route path="/filmmaker" element={ <Filmmaker/>} />
     <Route path="/login" element={ <Login/>} />
     <Route path="/register" element={ <Register/>} />
+    <Route path="/filmviewer" element={ <Filmviewer/>} />
   </Routes>
 }
 
---
+++ frontend/examsfront/src/pages/filmviewer.js
@@ -0,0 +1,13 @@
+
+function filmviewer()
+{
+    return (<div>
+        <h2>Moviesa and films</h2>
+       <video width="700px" height="400px" controls>
+            <source src="https://d2s3hb6ysmsfq9.cloudfront.net/Build%20a%20Video%20Streaming%20Service%20like%20YouTube,%20Netflix%20using%20AWS%20S3,%20CloudFront%20and%20React.mp4"/>
+        </video>
+        </div>)
+}
+export default filmviewer
+
+
