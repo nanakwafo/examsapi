b7395ebc5103deddf34400eae85b1aa3b7994d04
nanakwafo <nanamensah1140@gmail.com>
2023-04-13 22:55:38 +0000

    Bug fix

--- backend/examsapi/config/db.config.js
+++ backend/examsapi/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports ={
-   db: 'mongodb+srv://nanamensah1140:AJ3qE0WTJAxD2YDL@subscribers.2i7exkm.mongodb.net/?retryWrites=true&w=majority' 
-   
-}
+   db: 'mongodb://admin:password@10.0.2.99:27017/ifma?authSource=admin'  
+  // db: 'mongodb+srv://nanamensah1140:AJ3qE0WTJAxD2YDL@subscribers.2i7exkm.mongodb.net/?retryWrites=true&w=majority' 
+  }
